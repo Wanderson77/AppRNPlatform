bf4229d222f236b456d24f668d85fa7c4fcd78b6
Wanderson Aldo <wanderson.aldo@gmail.com>
2020-01-09 15:49:24 -0200

    Commit component

---
+++ src/components/platform.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text, Platform } from 'react-native';
+ 
+export default class App extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+        }}>
+        <Text
+          style={{
+            fontSize: 24,
+            textAlign: 'center',
+          }}>
+          {/*Here is Platform to get device type*/}
+          {Platform.OS === 'ios' ? 'Device is IOS' : 'Device is Android'}
+        </Text>
+      </View>
+    );
+  }
+}
